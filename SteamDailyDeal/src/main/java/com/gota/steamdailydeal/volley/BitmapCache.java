7676d968132c9a4a638dcf4cd2d3e98c201be262
Gota <g.tianixiang@gmail.com>
2014-06-01 12:08:48 +0800

    Add small size layout (When resize to one row)

--- SteamDailyDeal/src/main/java/com/gota/steamdailydeal/volley/BitmapCache.java
+++ SteamDailyDeal/src/main/java/com/gota/steamdailydeal/volley/BitmapCache.java
@@ -1,9 +1,15 @@
 package com.gota.steamdailydeal.volley;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.util.LruCache;
 
+import com.android.volley.Cache;
+import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.ImageLoader;
+import com.gota.steamdailydeal.App;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by Gota on 2014/5/17.
@@ -12,6 +18,7 @@ import com.android.volley.toolbox.ImageLoader;
 public class BitmapCache implements ImageLoader.ImageCache {
 
     private LruCache<String, Bitmap> mCache;
+    private DiskBasedCache mDiskCache;
 
     public BitmapCache() {
         int maxSize = 10 * 1024 * 1024;
@@ -21,15 +28,33 @@ public class BitmapCache implements ImageLoader.ImageCache {
                 return value.getRowBytes() * value.getHeight();
             }
         };
+        mDiskCache = new DiskBasedCache(App.instance.getCacheDir(), maxSize * 3);
+        mDiskCache.initialize();
     }
 
     @Override
     public Bitmap getBitmap(String url) {
-        return mCache.get(url);
+        Bitmap bitmap = mCache.get(url);
+        if (bitmap != null) return bitmap;
+
+        Cache.Entry entry = mDiskCache.get(url);
+        if (entry != null) {
+            return BitmapFactory.decodeByteArray(entry.data, 0, entry.data.length);
+        }
+
+        return null;
     }
 
     @Override
     public void putBitmap(String url, Bitmap bitmap) {
         mCache.put(url, bitmap);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        boolean ok = bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        if (ok) {
+            Cache.Entry entry = new Cache.Entry();
+            entry.data = baos.toByteArray();
+            mDiskCache.put(url, entry);
+        }
     }
 }
