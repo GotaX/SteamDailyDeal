addc1f9343b96c158ab0cb3d6ca4938a535298e4
Gota <g.tianixiang@gmail.com>
2014-06-01 13:01:57 +0800

    Put layout logical into LayoutBuilder

--- SteamDailyDeal/src/main/java/com/gota/steamdailydeal/DailyDealWidget.java
+++ SteamDailyDeal/src/main/java/com/gota/steamdailydeal/DailyDealWidget.java
@@ -1,23 +1,14 @@
 package com.gota.steamdailydeal;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.RemoteViews;
 
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
-import com.gota.steamdailydeal.constants.Steam;
-import com.gota.steamdailydeal.data.DataProvider;
-import com.gota.steamdailydeal.data.Tables;
-import com.gota.steamdailydeal.util.MyTextUtils;
+import com.gota.steamdailydeal.ui.LayoutBuilder;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -54,8 +45,9 @@ public class DailyDealWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Log.d(App.TAG, "on update: " + Arrays.toString(appWidgetIds));
+        LayoutBuilder lb = new LayoutBuilder(context, appWidgetManager);
         for (int appWidgetId : appWidgetIds) {
-            RemoteViews views = buildLargeLayout(context, appWidgetId);
+            RemoteViews views = lb.buildMediumLayout(appWidgetId);
             if (views == null) continue;
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -108,122 +100,29 @@ public class DailyDealWidget extends AppWidgetProvider {
                 appWidgetId, minWidth, maxWidth, minHeight, maxHeight));
         Log.d(App.TAG, "screen size: " + screenSize);
 
-        RemoteViews views = buildLayout(context, appWidgetManager, appWidgetId);
+        LayoutBuilder layoutBuilder = new LayoutBuilder(context, appWidgetManager);
+        RemoteViews views = buildLayout(layoutBuilder, appWidgetId);
         if (views != null) {
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
-    private RemoteViews buildSmallLayout(final Context context,
-                                         final AppWidgetManager appWidgetManager, final int appWidgetId) {
-        ContentResolver cr = context.getContentResolver();
-        Uri uri = Uri.withAppendedPath(DataProvider.CONTENT_URI, DataProvider.PATH_DAILY_DEAL);
-        Cursor cursor = cr.query(uri, Tables.SQL.PROJECTION_DAILY_DEAL_SMALL, null, null, null);
-
-        if (cursor.moveToFirst()) {
-            final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_small_item);
-
-            int id = cursor.getInt(cursor.getColumnIndex(Tables.TDeals.ID));
-            String headerImage = Steam.getMediumPic(id);
-            String currency = cursor.getString(cursor.getColumnIndex(Tables.TDeals.CURRENCY));
-            CharSequence name = cursor.getString(cursor.getColumnIndex(Tables.TDeals.NAME));
-            CharSequence discountPercent = MyTextUtils.getDiscount(
-                    cursor.getString(cursor.getColumnIndex(Tables.TDeals.DISCOUNT_PERCENT)));
-            int op = cursor.getInt(cursor.getColumnIndex(Tables.TDeals.ORIGINAL_PRICE));
-            CharSequence originalPrice = MyTextUtils.strikethrough(MyTextUtils.getCurrency(op, currency));
-            int fp = cursor.getInt(cursor.getColumnIndex(Tables.TDeals.FINAL_PRICE));
-            CharSequence finalPrice = MyTextUtils.getCurrency(fp, currency);
-
-            App.imgLoader.get(headerImage, new ImageLoader.ImageListener() {
-                @Override
-                public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
-                    views.setImageViewBitmap(R.id.img_header, imageContainer.getBitmap());
-                    appWidgetManager.partiallyUpdateAppWidget(appWidgetId, views);
-                }
-                @Override
-                public void onErrorResponse(VolleyError volleyError) {
-                    Log.e(App.TAG, "Error on request small image!", volleyError);
-                    views.setImageViewResource(R.id.img_header, R.drawable.not_found);
-                    appWidgetManager.partiallyUpdateAppWidget(appWidgetId, views);
-                }
-            });
-            views.setTextViewText(R.id.tv_name, name);
-            views.setTextViewText(R.id.tv_discount_percent, discountPercent);
-            views.setTextViewText(R.id.tv_original_price, originalPrice);
-            views.setTextViewText(R.id.tv_price, finalPrice);
-
-            views.setOnClickPendingIntent(R.id.btn_refresh, createRefreshPendingIntent(context));
-            views.setOnClickPendingIntent(R.id.img_header, createHeaderImagePendingIntent(context));
-            return views;
-        } else {
-            // TODO: Return empty view
-            return null;
-        }
-    }
-
-    private RemoteViews buildMediumLayout(Context context, int appWidgetId) {
-        RemoteViews views = newRemoteViews();
-        Intent intent = new Intent(context, MyWidgetService.class);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-        views.setRemoteAdapter(R.id.vf_main, intent);
-
-        views.setEmptyView(R.id.vf_main, R.id.empty);
-
-        // Init refresh button
-        Intent refreshIntent = new Intent(context, DailyDealWidget.class);
-        refreshIntent.setAction(ACTION_REFRESH);
-        PendingIntent piRefresh = PendingIntent.getBroadcast(
-                context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.btn_refresh, piRefresh);
-
-        // Init onclick image
-        Intent ivf = new Intent(Intent.ACTION_VIEW);
-        PendingIntent pivf = PendingIntent.getActivity(
-                context, 0, ivf, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setPendingIntentTemplate(R.id.vf_main, pivf);
-        return views;
-    }
-
-    private RemoteViews buildLayout(Context context,
-                                    AppWidgetManager appWidgetManager, int appWidgetId) {
+    private RemoteViews buildLayout(LayoutBuilder layoutBuilder, int appWidgetId) {
         Size screenSize = mSizeMap.get(appWidgetId);
         if (screenSize == null) {
             return null;
         } else switch (screenSize) {
             case SMALL:
-                return buildSmallLayout(context, appWidgetManager, appWidgetId);
+                return layoutBuilder.buildSmallLayout(appWidgetId);
             case MEDIUM:
-                return buildMediumLayout(context, appWidgetId);
+                return layoutBuilder.buildMediumLayout(appWidgetId);
             case LARGE:
-                return buildLargeLayout(context, appWidgetId);
+                return layoutBuilder.buildLargeLayout(appWidgetId);
             default:
                 return null;
         }
     }
 
-    private RemoteViews buildLargeLayout(Context context, int appWidgetId) {
-        return null;
-    }
-
-    private RemoteViews newRemoteViews() {
-        return new RemoteViews(App.instance.getPackageName(), R.layout.widget_main);
-    }
-
-    private PendingIntent createRefreshPendingIntent(Context context) {
-        Intent refreshIntent = new Intent(context, DailyDealWidget.class);
-        refreshIntent.setAction(ACTION_REFRESH);
-        PendingIntent pi = PendingIntent.getBroadcast(
-                context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        return pi;
-    }
-
-    private PendingIntent createHeaderImagePendingIntent(Context context) {
-        Intent ivf = new Intent(Intent.ACTION_VIEW);
-        PendingIntent pi = PendingIntent.getActivity(
-                context, 0, ivf, PendingIntent.FLAG_UPDATE_CURRENT);
-        return pi;
-    }
 }
 
 
---
+++ SteamDailyDeal/src/main/java/com/gota/steamdailydeal/ui/LayoutBuilder.java
@@ -0,0 +1,126 @@
+package com.gota.steamdailydeal.ui;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.gota.steamdailydeal.App;
+import com.gota.steamdailydeal.DailyDealWidget;
+import com.gota.steamdailydeal.MyWidgetService;
+import com.gota.steamdailydeal.R;
+import com.gota.steamdailydeal.constants.Steam;
+import com.gota.steamdailydeal.data.DataProvider;
+import com.gota.steamdailydeal.data.Tables;
+import com.gota.steamdailydeal.util.MyTextUtils;
+
+/**
+ * Created by Gota on 2014/6/1.
+ * Email: G.tianxiang@gmail.com
+ */
+public class LayoutBuilder {
+
+    private Context mContext;
+    private AppWidgetManager mAppWidgetManager;
+
+    public LayoutBuilder(Context context, AppWidgetManager appWidgetManager) {
+        this.mContext = context;
+        this.mAppWidgetManager = appWidgetManager;
+    }
+
+    public RemoteViews buildSmallLayout(final int appWidgetId) {
+        ContentResolver cr = mContext.getContentResolver();
+        Uri uri = Uri.withAppendedPath(DataProvider.CONTENT_URI, DataProvider.PATH_DAILY_DEAL);
+        Cursor cursor = cr.query(uri, Tables.SQL.PROJECTION_DAILY_DEAL_SMALL, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            final RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.app_small_item);
+
+            int id = cursor.getInt(cursor.getColumnIndex(Tables.TDeals.ID));
+            String headerImage = Steam.getMediumPic(id);
+            String storeLink = Steam.getStoreLink(id);
+            String currency = cursor.getString(cursor.getColumnIndex(Tables.TDeals.CURRENCY));
+            CharSequence name = cursor.getString(cursor.getColumnIndex(Tables.TDeals.NAME));
+            CharSequence discountPercent = MyTextUtils.getDiscount(
+                    cursor.getString(cursor.getColumnIndex(Tables.TDeals.DISCOUNT_PERCENT)));
+            int op = cursor.getInt(cursor.getColumnIndex(Tables.TDeals.ORIGINAL_PRICE));
+            CharSequence originalPrice = MyTextUtils.strikethrough(MyTextUtils.getCurrency(op, currency));
+            int fp = cursor.getInt(cursor.getColumnIndex(Tables.TDeals.FINAL_PRICE));
+            CharSequence finalPrice = MyTextUtils.getCurrency(fp, currency);
+
+            App.imgLoader.get(headerImage, new ImageLoader.ImageListener() {
+                @Override
+                public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
+                    views.setImageViewBitmap(R.id.img_header, imageContainer.getBitmap());
+                    mAppWidgetManager.partiallyUpdateAppWidget(appWidgetId, views);
+                }
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    Log.e(App.TAG, "Error on request small image!", volleyError);
+                    views.setImageViewResource(R.id.img_header, R.drawable.not_found);
+                    mAppWidgetManager.partiallyUpdateAppWidget(appWidgetId, views);
+                }
+            });
+            views.setTextViewText(R.id.tv_name, name);
+            views.setTextViewText(R.id.tv_discount_percent, discountPercent);
+            views.setTextViewText(R.id.tv_original_price, originalPrice);
+            views.setTextViewText(R.id.tv_price, finalPrice);
+
+            views.setOnClickPendingIntent(R.id.btn_refresh, createRefreshPendingIntent());
+            views.setOnClickPendingIntent(R.id.img_header, createHeaderImagePendingIntent(storeLink));
+            return views;
+        } else {
+            // TODO: Return empty view
+            return null;
+        }
+    }
+
+    public RemoteViews buildMediumLayout(final int appWidgetId) {
+        RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.widget_main);
+        Intent intent = new Intent(mContext, MyWidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        views.setRemoteAdapter(R.id.vf_main, intent);
+
+        views.setEmptyView(R.id.vf_main, R.id.empty);
+
+        // Init refresh button
+        Intent refreshIntent = new Intent(mContext, DailyDealWidget.class);
+        refreshIntent.setAction(DailyDealWidget.ACTION_REFRESH);
+        PendingIntent piRefresh = PendingIntent.getBroadcast(
+                mContext, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.btn_refresh, piRefresh);
+
+        // Init onclick image
+        Intent ivf = new Intent(Intent.ACTION_VIEW);
+        PendingIntent pi = PendingIntent.getActivity(
+                mContext, 0, ivf, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.vf_main, pi);
+        return views;
+    }
+
+    public RemoteViews buildLargeLayout(final int appWidgetId) {
+        return null;
+    }
+
+    private PendingIntent createRefreshPendingIntent() {
+        Intent refreshIntent = new Intent(mContext, DailyDealWidget.class);
+        refreshIntent.setAction(DailyDealWidget.ACTION_REFRESH);
+        return PendingIntent.getBroadcast(
+                mContext, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private PendingIntent createHeaderImagePendingIntent(String url) {
+        Intent ivf = new Intent(Intent.ACTION_VIEW);
+        ivf.setData(Uri.parse(url));
+        return PendingIntent.getActivity(
+                mContext, 0, ivf, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+}
